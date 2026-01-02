16eec522133f0e116d1470bc82cf2153f51d1b38
Apricosma <113317992+Apricosma@users.noreply.github.com>
2026-01-02 03:50:43 -0600

    Add ThemeProvider component and integrate it into the App component

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import "./App.css";
 import Sidebar from "./Components/Sidebar";
 import ActiveViewContent from "./Components/ActiveViewContent";
 import { ViewType } from "./Components/ActiveViewContent";
+import { ThemeProvider } from "./Components/theme-provider";
 
 function App() {
   const [activeView, setActiveView] = useState<ViewType>(ViewType.Home);
@@ -16,17 +17,19 @@ function App() {
   ];
 
   return (
-    <div className="flex min-h-screen bg-appbg">
-      {/* Sidebar and Main Dock Area */}
-      <div className="flex flex-col w-64 flex-shrink-0 bg-appbg text-white">
-        <Sidebar items={sidebarItems} />
-      </div>
+    <ThemeProvider defaultTheme="dark">
+      <div className="flex min-h-screen bg-background">
+        {/* Sidebar and Main Dock Area */}
+        <div className="flex flex-col w-64 flex-shrink-0 bg-appbg text-white">
+          <Sidebar items={sidebarItems} />
+        </div>
 
-      {/* Main App Section */}
-      <main className="flex-grow container mx-auto px-4 py-2">
-        <ActiveViewContent activeView={activeView} />
-      </main>
-    </div>
+        {/* Main App Section */}
+        <main className="flex-grow container mx-auto px-4 py-2">
+          <ActiveViewContent activeView={activeView} />
+        </main>
+      </div>
+    </ThemeProvider>
   );
 }
 
